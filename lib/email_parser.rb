6bbdc97766ba8c727568066aca8074ddf7cd37c6
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-12 18:37:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -17,3 +17,17 @@ class EmailAddressParser
   end
     
 end
+class EmailParser
+
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
+  end
+
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
+end
